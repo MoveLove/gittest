9aa7f0c91c424614f03db67f18b33693f5b20228
MoveLove <1967409028@qq.com>
2019-08-24 18:27:56 +0800

    提交到GitHub修改Test第7行

--- demo/src/main/java/com/itcast/jopo/Test.java
+++ demo/src/main/java/com/itcast/jopo/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("save");
         System.out.println("新加");
+        System.out.println("新加1");
     }
 }
